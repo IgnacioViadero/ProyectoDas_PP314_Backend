b1042f202da99659c1d2eb70be965e447a3acfef
IgnacioViadero <202206059@alu.comillas.edu>
2025-05-15 18:56:13 +0200

    Backend Terminado

--- backend/auctionsBackend/auctionsBackend/wsgi.py
+++ backend/auctionsBackend/auctionsBackend/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'auctions.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'auctionsBackend.settings')
 
 application = get_wsgi_application()
